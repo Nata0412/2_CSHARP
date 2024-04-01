639b182582513d5f51879704c7b7b9dd99b8c74d
Natallia Muslimova <nat-9@mail.ru>
2024-04-01 21:11:21 +0300

    add Lesson2:Task3: Program

--- Lesson2/Task 3/Program.cs
+++ Lesson2/Task 3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 10;
+int[] array = { 2, 5, 4, 7, 8, 9, 35, 8, 3, 9 };
+int i = 0;
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+    //Console.Write(arr[i]);
+    //Console.Write(' ');
+    Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+
+}
