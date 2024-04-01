f9464dd7c2e58aaed8a850dd34287c3c11c059af
Natallia Muslimova <nat-9@mail.ru>
2024-04-01 21:41:04 +0300

    add Lesson2: Task4: program

--- Lesson2/Task 4/Program.cs
+++ Lesson2/Task 4/Program.cs
@@ -11,3 +11,21 @@ while (i < n)
     i = i + 1;
 }
 Console.Write(max);
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+max = array[0]; 
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
